5e8df390f199681ec1afe6b61cb71da0e81891c6
sincere-gangly <sincere-gangly0y@icloud.com>
2024-07-17 08:20:01 +0500

    Refactored error handling logic

---
+++ process.js
@@ -0,0 +1,23 @@
+class BinaryHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      let element = this.heap[index];
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.heap[parentIndex];
+      if (parent >= element) break;
+      this.heap[index] = parent;
+      this.heap[parentIndex] = element;
+      index = parentIndex;
+    }
+  }
+}
