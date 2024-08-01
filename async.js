43c77e49fda22332a9a996df946a5157ec85ce2f
sincere-gangly <sincere-gangly0y@icloud.com>
2024-08-01 19:35:00 +0500

    Improved logging for better traceability

---
+++ async.js
@@ -0,0 +1,1 @@
+const throttle = (fn, limit) => { let inThrottle; return function(...args) { if (!inThrottle) { fn.apply(this, args); inThrottle = true;setTimeout(() => inThrottle = false, limit); } }; };
